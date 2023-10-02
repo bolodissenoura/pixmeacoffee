43ae78b26224f24e7d720f6476007d5ef98c95db
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-10-02 00:54:46 -0300

    feat: connet env with client side

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,22 +1,20 @@
-// Import the functions you need from the SDKs you need
+"use client";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 
-
 // TODO: Connect with .env
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
