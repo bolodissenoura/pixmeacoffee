016b6acbb81c10a911bad3db916169799cea48e3
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-10-04 23:58:45 -0300

    feat: onClick img goto dashboard

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { UserAuth } from "@/app/context/AuthContext";
+import Link from "next/link";
 
 export default function NavBar() {
   const { user, logOut } = UserAuth();
@@ -39,21 +40,23 @@ export default function NavBar() {
         ) : (
           <>
             <div className="flex gap-4">
-              <Image
-                src={user?.photoURL}
-                alt="Logo - copo de café azul com logo do pix"
-                data-dropdown-toggle="userDropdown"
-                data-dropdown-placement="bottom-start"
-                className="w-10 h-10 rounded-full cursor-pointer"
-                width={60}
-                height={60}
-                priority
-              />
+              <Link href="/dashboard">
+                <Image
+                  src={user?.photoURL}
+                  alt="Logo - copo de café azul com logo do pix"
+                  data-dropdown-toggle="userDropdown"
+                  data-dropdown-placement="bottom-start"
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                  width={60}
+                  height={60}
+                  priority
+                />
+              </Link>
               <button onClick={handleLogOut}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
+                  width="18"
+                  height="18"
                   fill="#111827"
                   viewBox="0 0 256 256">
                   <path d="M112,216a8,8,0,0,1-8,8H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32h56a8,8,0,0,1,0,16H48V208h56A8,8,0,0,1,112,216Zm109.66-93.66-40-40a8,8,0,0,0-11.32,11.32L196.69,120H104a8,8,0,0,0,0,16h92.69l-26.35,26.34a8,8,0,0,0,11.32,11.32l40-40A8,8,0,0,0,221.66,122.34Z"></path>
