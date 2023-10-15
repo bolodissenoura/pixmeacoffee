b93caef5042d80b96bc7a0bfdef108c7a428c942
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-10-14 21:08:59 -0300

    feat: button to publish booleand hidden logic

--- src/components/PublishBtn.tsx
+++ src/components/PublishBtn.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-export default function PublishBtn() {
+interface PublishBtnInterface {
+  handleSubmitForm: () => void;
+  showButton: boolean;
+}
+
+export default function PublishBtn(props: PublishBtnInterface) {
   return (
-    <div className="w-full hidden h-20 fixed bottom-0 left-0 bg-white p-4">
+    <div
+      className={`w-full h-20 fixed bottom-0 left-0 bg-white p-4 ${
+        !props.showButton ?? "hidden"
+      }`}>
       <button
         type="submit"
+        onClick={props.handleSubmitForm}
         className="bg-primary-500 w-full h-full shadow text-white">
         Publicar
       </button>
