ec9c720c77a79867bda73946d58ddb6055a541e4
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-10-23 18:09:54 -0300

    feat: get the pagename by the user page name

--- src/app/[namepage]/page.tsx
+++ src/app/[namepage]/page.tsx
@@ -1,6 +1,34 @@
 "use client";
 import React from "react";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
 export default function Profile() {
-  return <div className="bg-primary-500">{window.location.pathname}</div>;
+  const db = getFirestore();
+  const usernameToSearch = window.location.pathname.substring(1);
+  const usersCollection = collection(db, "users");
+  const q = query(usersCollection, where("namepage", "==", usernameToSearch));
+
+  React.useEffect(() => {
+    getDocs(q)
+      .then((querySnapshot) => {
+        if (querySnapshot.size === 0) {
+          console.log("Nenhum usuário encontrado com esse username.");
+        } else {
+          querySnapshot.forEach((doc) => {
+            console.log("Dados do usuário:", doc.data());
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar usuário:", error);
+      });
+    getDocs;
+  });
+  return <div className="bg-primary-500">{usernameToSearch}</div>;
 }
