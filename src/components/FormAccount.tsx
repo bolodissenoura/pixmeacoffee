5ee759e1c7bfbb9a127b1dcfbf30da3501bf5b31
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-11-06 23:18:14 -0300

    feat: paste to clipboard button

--- src/components/FormAccount.tsx
+++ src/components/FormAccount.tsx
@@ -103,6 +103,16 @@ export default function FormAccount(props: FormAccountInterface) {
     }
   };
   const n = 9;
+  const pasteFromClipboard = () => {
+    navigator.clipboard
+      .readText()
+      .then((text) => {
+        props.data.setPixKey(text);
+      })
+      .catch((err) => {
+        console.error("Failed to read clipboard contents: ", err);
+      });
+  };
   return (
     <div className="p-4 w-80 md:w-8/12 md:h-96 bg-white md:mt-16 rounded-2xl overflow-hidden">
       <div className="md:flex-row flex-col flex w-full">
@@ -124,7 +134,8 @@ export default function FormAccount(props: FormAccountInterface) {
                 props.data?.setStatus({
                   ...props.data?.status,
                   pageStatus: "error",
-                  pageStatusMsg: "Toque em qualquer lugar fora deste campo para verificar.",
+                  pageStatusMsg:
+                    "Toque em qualquer lugar fora deste campo para verificar.",
                 });
               }}
               className={`border border-solid ${inputColor(
@@ -148,18 +159,32 @@ export default function FormAccount(props: FormAccountInterface) {
               className="block mb-2 text-sm font-medium text-gray-900">
               chave aleatória pix:
             </label>
-            <input
-              type="text"
-              id="pixkey"
-              onChange={(e) => props.data.setPixKey(e.target.value)}
-              value={props.data.pixKey}
-              className={`bg-gray-50 border ${inputColor(
-                props.data.status?.pageStatus,
-                "border"
-              )} border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-10/12 p-2.5`}
-              placeholder="xxx-xxx-xxx-xxx..."
-              required
-            />
+            <div className="flex gap-2 ">
+              <input
+                type="text"
+                id="pixkey"
+                onChange={(e) => props.data.setPixKey(e.target.value)}
+                value={props.data.pixKey}
+                className={`bg-gray-50 border ${inputColor(
+                  props.data.status?.pageStatus,
+                  "border"
+                )} border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-8/12 p-2.5`}
+                placeholder="xxx-xxx-xxx-xxx..."
+                required
+              />
+              <button
+                onClick={pasteFromClipboard}
+                className="border rounded border-gray-300 p-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="#000000"
+                  viewBox="0 0 256 256">
+                  <path d="M200,32H163.74a47.92,47.92,0,0,0-71.48,0H56A16,16,0,0,0,40,48V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V48A16,16,0,0,0,200,32Zm-72,0a32,32,0,0,1,32,32H96A32,32,0,0,1,128,32Zm72,184H56V48H82.75A47.93,47.93,0,0,0,80,64v8a8,8,0,0,0,8,8h80a8,8,0,0,0,8-8V64a47.93,47.93,0,0,0-2.75-16H200Z"></path>
+                </svg>
+              </button>
+            </div>
             <p
               className={`${inputColor(
                 props.data.status?.pageStatus,
